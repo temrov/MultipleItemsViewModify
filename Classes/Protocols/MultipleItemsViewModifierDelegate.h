39a40c50a81a3d6f6b8307c0fc4868f712f6a38e
Vadim Temnogrudov <vtemnogrudov@fun.co>
2018-02-20 21:43:36 +0300

    Protocol files add

---
+++ Classes/Protocols/MultipleItemsViewModifierDelegate.h
@@ -0,0 +1,18 @@
+//
+//  MultipleItemsViewModifierDelegate.h
+//
+//  Created by Vadim Temnogrudov on 20/02/2018.
+//
+
+@protocol MultipleItemsViewModifierProtocol;
+
+@protocol MultipleItemsViewModifierDelegate
+
+@optional
+// called directly to modifications of view
+- (void)modifier:(nonnull NSObject<IFMultipleItemsViewModifierProtocol> *)modifier willUpdateView:(nullable UIView *)view;
+
+// called immediately after modification of view
+- (void)modifier:(nonnull NSObject<IFMultipleItemsViewModifierProtocol> *)modifier didUpdatedView:(nullable UIView *)view;
+
+@end
---
+++ Classes/Protocols/MultipleItemsViewModifierProtocol.h
@@ -0,0 +1,40 @@
+//
+//  MultipleItemsViewModifierProtocol.h
+//
+//  Created by Vadim Temnogrudov on 20/02/2018.
+//
+
+/**
+ * Prototype of modification function.
+ * Body of function used to perform model modifications.
+ * Result of this function is array of items (their index paths)
+ * that had been modified in this block and should been modified in view.
+ */
+typedef NSArray<NSIndexPath *> *_Nonnull (^multipleItemsViewModifyBlock)(void);
+
+
+/**
+ * Controller-side object that performs routine update/delete/insert
+ * operations with multiple items views (like UITableView or UICollectionView).
+ * Object allows performing safe modifications of view and model atomically
+ * that prevents from inconsistency crashes.
+ */
+@protocol MultipleItemsViewModifierProtocol
+
+@property (nullable, nonatomic, weak) NSObject<IFMultipleItemsViewModifierDelegate> *delegate;
+
+/**
+ * Method performs animated model and view modifications atomically.
+ * Modification order:
+ * 1. existing items are updating
+ * 2. exhausted items are deleting
+ * 3. new items are inserting.
+ */
+- (void)modifyAnimatedWithUpdateBlock:(nullable multipleItemsViewModifyBlock)updateBlock
+                          deleteBlock:(nullable multipleItemsViewModifyBlock)deleteBlock
+                          insertBlock:(nullable multipleItemsViewModifyBlock)insertBlock;
+
+// Atomically view and model modifying without any animation
+- (void)modifyNotAnimatedWithBlock:(void (^_Nullable)(void))modifyBlock;
+
+@end
