616b60490bc7e46e7de9a73ae486a2fe0c9b6510
Vadim Temnogrudov <vtemnogrudov@fun.co>
2018-02-20 23:15:24 +0300

    Fix

--- Classes/UITableView/TableViewModifier.h
+++ Classes/UITableView/TableViewModifier.h
@@ -3,9 +3,8 @@
 //
 //  Created by Vadim Temnogrudov on 20/02/2018.
 //
-
-#import <Foundation/Foundation.h>
 #import "MultipleItemsViewModifierProtocol.h"
+#import <Foundation/Foundation.h>
 
 @interface TableViewModifier : NSObject<MultipleItemsViewModifierProtocol>
 
