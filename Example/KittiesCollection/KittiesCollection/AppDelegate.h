870e06734dfd778cac7b99bd896a0d11e3cf4e95
Vadim Temnogrudov <vtemnogrudov@fun.co>
2018-02-22 21:27:38 +0300

    Empty example project

---
+++ Example/KittiesCollection/KittiesCollection/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KittiesCollection
+//
+//  Created by Vadim Temnogrudov on 22/02/2018.
+//  Copyright © 2018 temrov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Example/KittiesCollection/KittiesCollection/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KittiesCollection
+//
+//  Created by Vadim Temnogrudov on 22/02/2018.
+//  Copyright © 2018 temrov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
